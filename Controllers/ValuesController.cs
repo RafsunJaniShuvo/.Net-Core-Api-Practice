a94f6154be9a74d4a18a72fee6d0145562c188e0
rafsun <rafsunjani@ba-systems.com>
2024-03-20 01:19:15 +0600

    Created .net core api practice and practiced routecustom name

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDotNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [Route("get")]
+        [HttpGet]
+        public string GetName()
+        {
+            return "Rafsun jani shuvo";
+        }
+    }
+}
