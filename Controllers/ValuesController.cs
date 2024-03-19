eda16f92a166cb2e15429f024ecfd1817507e60a
rafsun <rafsunjani@ba-systems.com>
2024-03-20 01:46:46 +0600

    add post model with annotation / validation

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,16 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiDotNetCore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")] // If I want to get all action using single route [best practice and short ]
     [ApiController]
     public class ValuesController : ControllerBase
     {
 
-        [Route("get")]
+        //[Route("get")]
+        //[Route("[action]")] // if I want to access action name
         [HttpGet]
         public string GetName()
         {
             return "Rafsun jani shuvo";
         }
+
     }
 }
---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDotNetCore.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Pleas Enter Title")] 
+        public string Title { get; set; }
+        [Required(ErrorMessage ="Please Enter Description")]
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
